1ec77be07fd4b2466437fa09e9f7132a9fa74e97
wys-prog <wys-workshop@outlook.fr>
2026-01-12 13:07:54 +0100

    this version is messed

--- Core/Version.cs
+++ Core/Version.cs
@@ -1,7 +1,13 @@
 namespace monoe.exe.Core;
 public static partial class Version {
     public static readonly int Major = 2;
+<<<<<<< HEAD
     public static readonly int Minor = 5;
     public static readonly int Fix = 18;
     public static readonly string All = "2.5.18";
+=======
+    public static readonly int Minor = 4;
+    public static readonly int Fix = 1;
+    public static readonly string All = "2.4.1";
+>>>>>>> 44ffe6c (this version is messed)
 }
